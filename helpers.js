824fba2249c2f6ccf7e00050012a78a475a736f8
bsteg17 <bsteg17@gmail.com>
2016-08-21 17:14:12 -0500

    successfully, if imperfectly, tokenizing articles sentence-by-sentence

--- helpers.js
+++ helpers.js
@@ -1,7 +1,26 @@
+// Extend Array
+
 Array.prototype.isEmpty = function() {
 	if (this.length == 0) { return true; } 
 	else                  { return false; }
 }
+
+// Extend HTMLCollection
+
+HTMLCollection.prototype.toArray = function() {
+	return Array.prototype.slice.call( this );
+}
+
+//Extend String
+
+String.prototype.splitButKeepSeperators = function (seperator) {
+	parts = this.split(seperator);
+	for (i= parts.length; i-->1;)
+    	parts.splice(i, 0, seperator);
+    return parts;
+}
+
+// misc
 
 function LOG(str) {
 	console.log(str);
--- script.js
+++ script.js
@@ -2,21 +2,36 @@ var articles;
 
 function detectArticles(callback) {
 	LOG("detecting articles...")
-	articles = document.getElementsByTagName("article");
-	if (articles.isEmpty) { return; }
+	articles = document.getElementsByTagName("article")
+	  .toArray()
+	  .map(function(article) {return {"element":article}});
+	if (articles.isEmpty()) { return; }
 	callback();
 }
 
-function tokenizeArticles() {
+function extractArticleText(callback) {
+	LOG("extracting article text from html...")
+	articles.forEach(function(article) {
+        article.text = article.element.textContent;
+	});
+	callback();
+}
 
+function tokenizeArticleText() {
+	LOG("tokenizing articles...")
+	articles.forEach(function(article) {
+		LOG(article.text)
+		article.sentences = article.text.splitButKeepSeperators(/[.!?"] /);
+		LOG(article.sentences);
+	});
 }
 
 function init() {
 	detectArticles(function() {
-	tokenizeArticles(function() {
+	extractArticleText( function() {
+	tokenizeArticleText(function() {
 	LOG(articles);
-	})
-	});
+	}) }) });
 }
 
 window.onload = function() {
